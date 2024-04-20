96af695600a2c86c623cf8e8cce7cecc417bfb29
SimoneChecchia <162876775+Simogithub02@users.noreply.github.com>
2024-04-20 11:30:28 +0200

    implementation starting card

--- src/main/java/it/polimi/ingsw/is24am03/StartingCard.java
+++ src/main/java/it/polimi/ingsw/is24am03/StartingCard.java
@@ -7,9 +7,26 @@ public class StartingCard extends PlayableCard{
     public ArrayList<Resources> getKingdomList() {
         return kingdomList;
     }
+    public StartingCard(int id, int points, Corner corner0, Corner corner1, Corner corner2, Corner corner3, Corner back,ArrayList<Resources> kingdomList){
+        super(id, points, corner0, corner1, corner2, corner3, back);
+        this.kingdomList=kingdomList;
+
+    }
 
     @Override
     public int getType() {
         return 0;
+    }
+    public ArrayList<Resources> getRequirements(){
+        return null;
+    }
+    public CornerItem getObject(){
+        return CornerItem.EMPTY;
+    }
+    public int getScoringType(){
+        return -1;
+    }
+    public CornerItem getKingdomsType(){
+        return CornerItem.EMPTY;
     }
 }
