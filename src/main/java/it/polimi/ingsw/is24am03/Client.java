66df4f897d4cfdcde12ed64fc1aa008c6da3a26d
Andrea Callari <andrea.callari@mail.polimi.it>
2024-05-12 00:59:28 +0200

    Client interface updated

--- src/main/java/it/polimi/ingsw/is24am03/Client.java
+++ src/main/java/it/polimi/ingsw/is24am03/Client.java
@@ -3,4 +3,6 @@ package it.polimi.ingsw.is24am03;
 public interface Client {
     public void CreateGame(int nPlayers, String nickname);
     public void JoinGame(String nickname);
+
+    void PickColor(String color);
 }
