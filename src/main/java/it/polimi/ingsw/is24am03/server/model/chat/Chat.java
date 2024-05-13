68934d5c321ad6b432c37c93bdcac291d959da06
federicacurrodossi <160788240+federicacurrodossi@users.noreply.github.com>
2024-05-13 02:22:59 +0200

    Update Chat

--- src/main/java/it/polimi/ingsw/is24am03/server/model/chat/Chat.java
+++ src/main/java/it/polimi/ingsw/is24am03/server/model/chat/Chat.java
@@ -1,20 +1,87 @@
 package it.polimi.ingsw.is24am03.server.model.chat;
 
+import it.polimi.ingsw.is24am03.Subscribers.ChatSub;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 public class Chat {
+    public ArrayList<ChatSub> getChatSubs() {
+        return chatSubs;
+    }
+
+    private ArrayList<ChatSub> chatSubs;
+
 
     //arraylist di messaggi che indicano chat di gruppo
+    private ArrayList<Text> texts;
+
 
     //attributo relativo al chat listener
+    // private ChatObserver chatObserver;
 
     //costruttore
+    public Chat(){
+        this.texts=new ArrayList<Text>();
+        this.chatSubs=new ArrayList<>();
+    }
 
-    //subscriber per chat listener
+    public void addSub(ChatSub chatSub){
+        this.chatSubs.add(chatSub);
+    }
 
     //metodo che invoca chat listener notificandola di un nuovo messaggio
 
-    //getter per chat listeneer
+    // in questo metodo posso controllare la validità del messaggio ricevuto
+    public void NotifyChat(Text t){
+        if(t.getRecipient().isEmpty()) {
+            //NOTIFICO TUTTI I SUB DI UN MESSAGGIO DELLA CHAT PUBBLICA
+            for (ChatSub chatSub : getChatSubs()) {
+                try {
+                    chatSub.ReceiveGroupText(t.getSender(), t.getMex());
 
-    //getter per messaggi da parte di un giocatore
+                } catch (RemoteException ignored) {
+                }
+            }
+            this.texts.add(t);
+        }
+        else {
+            //NOTIFICO SOLO IL SUB INTERESSATO DELLA CHAT PRIVATA
+            for(ChatSub chatSub: getChatSubs()){
+                try{
+                    if(chatSub.getSub().equals(t.getRecipient())){
+                        chatSub.ReceivePrivateText(t.getSender(),t.getRecipient(),t.getMex());
+                        break;
+                    }
+                }catch (RemoteException ignored){}
+            }
+            this.texts.add(t);
+        }
+    }
+    //return messages sent to a player - broadcast messages
+    public ArrayList<Text> getGroupChat(String player){
+        ArrayList<Text> groupChat= new ArrayList<>();
+        for(Text t: getTexts()){
+            if(t.getRecipient()==null){
+                groupChat.add(t);
+            }
+        }
+        return groupChat;
+    }
 
+    //return messages sent by a player from another player (only private messages)
+    public ArrayList<Text> getPrivateChat(String receiver, String sender){
+        ArrayList<Text> privateChat= new ArrayList<>();
+        for(Text t: getTexts()){
+            if((t.getRecipient().equals(receiver) && t.getSender().equals(sender))||(t.getRecipient().equals(sender) && t.getSender().equals(receiver))){
+                privateChat.add(t);
+            }
+        }
+        return privateChat;
 
+    }
+
+    public ArrayList<Text> getTexts() {
+        return texts;
+    }
 }
--- src/main/java/it/polimi/ingsw/is24am03/server/model/chat/Text.java
+++ src/main/java/it/polimi/ingsw/is24am03/server/model/chat/Text.java
@@ -2,14 +2,39 @@ package it.polimi.ingsw.is24am03.server.model.chat;
 
 public class Text {
 
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getMex() {
+        return mex;
+    }
+
     //stringa mittente
+    private String sender;
+
 
     //stringa destinatario
+    private String recipient;
 
     //stringa messaggio
+    private String mex;
 
     //metodo per costruire messaggio privato
-
+    public Text(String sender, String mex){
+        this.sender=sender;
+        this.recipient=null;
+        this.mex=mex;
+    }
     //metodo per costruire messaggio di gruppo
+    public Text(String sender, String recipient,String mex){
+        this.sender=sender;
+        this.recipient=recipient;
+        this.mex=mex;
+    }
 
 }
